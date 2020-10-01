5de3ae0e21b69fc0d3e8de379ff26f86a9c842e3
WhiteRose7-pretty <cholpine@outlook.com>
2020-10-02 01:51:55 +0800

    db change in setting

--- app_rama/settings.py
+++ app_rama/settings.py
@@ -71,9 +71,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app_rama.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
